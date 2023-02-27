06b56b9a616d2f31043553f112b0eb9c581737ef
unknown <harsh@orientaloutsourcing.com>
2023-02-27 09:41:29 +0530

    Add_new

--- src/components/Config/Mockdata.js
+++ src/components/Config/Mockdata.js
@@ -1,28 +1,27 @@
 export const drawerData = [
-    {
-        val: "Dashboard",
-        isActive: false,
-        Routes: "/dashboard",
-    },
-    {
-        val: "Employees",
-        isActive: false,
-        Routes: "/employees",
-    },
-    {
-        val: "Tasks",
-        isActive: false,
-        Routes: "/tasks",
-    },
-    {
-        val: "OnGoing Projects",
-        isActive: false,
-        Routes: "/ongoingprojects",
-    },
-    {
-        val: "Closed Projects",
-        isActive: false,
-        Routes: "/closedprojects",
-    },
-
-]
+  {
+    val: "Dashboard",
+    isActive: false,
+    Routes: "/dashboard",
+  },
+  {
+    val: "Employees",
+    isActive: false,
+    Routes: "/employees",
+  },
+  {
+    val: "Tasks",
+    isActive: false,
+    Routes: "/tasks",
+  },
+  {
+    val: "OnGoing Projects",
+    isActive: false,
+    Routes: "/ongoingprojects",
+  },
+  {
+    val: "Closed Projects",
+    isActive: false,
+    Routes: "/closedprojects",
+  },
+];
